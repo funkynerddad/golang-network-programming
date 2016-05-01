43f5fc7188b5c34e4d7ed4914cb3db65e7d65754
funkynerddad <chiefnerd@funkynerddad.com>
2016-04-30 23:05:21 -0500

    chap 3.3

---
+++ Mask.go
@@ -0,0 +1,35 @@
+// Mask
+
+package mask
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s dotted-ip-addr\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	dotAddr := os.Args[1]
+
+	addr := net.ParseIP(dotAddr)
+	if addr == nil {
+		fmt.Println("Invalid Address")
+		os.Exit(0)
+	}
+	mask := addr.DefaultMask()
+	network := addr.Mask(mask)
+
+	ones, bits := mask.Size()
+	fmt.Println("Address is ", addr.String(),
+		" Default Mask length is ", bits,
+		" Leading ones count is ", ones,
+		" Mask is (hex) ", mask.String(),
+		" Network is ", network.String())
+	os.Exit(0)
+}
